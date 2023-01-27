e4f30cb0b1c8184a0ec5441e79647b70654b9566
Dustin Jones <dustintaylorjones@gmail.com>
2023-01-27 15:48:46 -0500

    feat: enable serving SVGs from CDN

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,7 +1,12 @@
 /* @flow */
 
-import { LOGO_COLOR } from "../constants";
-import type { LogoColorMap, LogoColors } from "../types";
+import { CDN_BASE_URL, LOGO, LOGO_COLOR } from "../constants";
+import type {
+  LogoColorMap,
+  LogoColors,
+  LogoColorSVGMap,
+  SVGGetter,
+} from "../types";
 
 export function getLogoColors(
   name: string,
@@ -25,4 +30,41 @@ export function getLogoColors(
   }
 
   return colors;
+}
+
+export function getSVGs(
+  logoName: $Values<typeof LOGO>,
+  svgGetter: SVGGetter,
+  logoColorMap: LogoColorMap
+): LogoColorSVGMap {
+  const svgs = {};
+
+  for (const logoColor of Object.keys(logoColorMap)) {
+    const logoColors = getLogoColors(logoName, logoColorMap, logoColor);
+
+    svgs[logoColor] = svgGetter(logoColors);
+  }
+
+  return svgs;
+}
+
+export function getSVGFilename(
+  logoName: $Values<typeof LOGO>,
+  logoColor: $Values<typeof LOGO_COLOR>
+): string {
+  return `${logoName}-${logoColor}.svg`;
+}
+
+export function getLogoCDNUrl(
+  logoName: $Values<typeof LOGO>,
+  logoColorMap: LogoColorMap,
+  logoColor: $Values<typeof LOGO_COLOR>
+): string {
+  if (!logoColorMap[logoColor]) {
+    logoColor = LOGO_COLOR.DEFAULT;
+  }
+
+  const svgFilename = getSVGFilename(logoName, logoColor);
+
+  return `${CDN_BASE_URL}/${svgFilename}`;
 }
